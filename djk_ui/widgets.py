790fbf13fd336c0e30b8a7c344ff3c025135520f
Dmitriy Sintsov <questpc256@gmail.com>
2022-06-04 22:34:11 +0300

    Set UiBaseGridWidget.template_id value.

--- djk_ui/widgets.py
+++ djk_ui/widgets.py
@@ -4,4 +4,5 @@ from django.forms.widgets import ChoiceWidget
 class UiBaseGridWidget(ChoiceWidget):
 
     js_classpath = 'FkGridWidget'
+    template_id = 'ko_fk_grid_widget'
     component_template_str = '<span{component_attrs}></span>'
